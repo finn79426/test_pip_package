9f3e6fca0355073834027e49d5c1d9dd454e26a2
finn79426 <finn79426@gmail.com>
2024-05-17 13:39:10 +0800

    up

--- setup.py
+++ setup.py
@@ -19,24 +19,36 @@
 
 
 
-from setuptools import setup
+# from setuptools import setup
 
-# with open("README.rst", "r") as fh:
-#     long_description = fh.read()
+# # with open("README.rst", "r") as fh:
+# #     long_description = fh.read()
+
+# setup(
+#     name="published",
+#     version="0.1.0",
+#     packages=["published",],
+#     install_requires=[],
+#     license="MIT",
+#     # url="https://pypi.org/project/published",
+#     author="python.supply",
+#     author_email="contact@python.supply",
+#     description="Example illustrating how an open-source library "+\
+#                 "can be organized and published.",
+#     long_description='long_description',
+#     long_description_content_type="text/x-rst",
+#     test_suite="nose.collector",
+#     tests_require=["nose"],
+# )
+
+
+
+from setuptools import setup, find_namespace_packages
 
 setup(
-    name="published",
-    version="0.1.0",
-    packages=["published",],
-    install_requires=[],
-    license="MIT",
-    # url="https://pypi.org/project/published",
-    author="python.supply",
-    author_email="contact@python.supply",
-    description="Example illustrating how an open-source library "+\
-                "can be organized and published.",
-    long_description='long_description',
-    long_description_content_type="text/x-rst",
-    test_suite="nose.collector",
-    tests_require=["nose"],
+    name="pure_python_package",
+    version="0.0.1",
+    python_requires='>=3',
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src')
 )
