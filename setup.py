9b6ee8f901917545deaa44963fc6531c7039f883
finn79426 <finn79426@gmail.com>
2024-05-17 12:35:26 +0800

    update

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='test_pip_package',
+    version='0.0.1',
+    author='FOOBAR',
+    author_email='foobar@gmail.com',
+    description='',
+    more_description='',
+    more_description_content_type='text/markdown',
+    url='',
+    project_urls={},
+    license='',
+    packages=['src', 'play'],
+    install_requires='',
+    python_requires=">=3.11"
+)
